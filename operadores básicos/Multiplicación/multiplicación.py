93b07ed494dbcf7424b2574a7c8846f392fdbc06
Say133 <99740761+Say133@users.noreply.github.com>
2022-03-03 17:21:20 -0500

    Multiplicación

---
+++ operadores básicos/Multiplicación/multiplicación.py
@@ -0,0 +1,25 @@
+##ejercicio 1
+````python
+m1=4
+m2=2
+print(m1*m2)
+output= 8
+````
+##ejercicio 2
+````python
+a = 10
+b = 4
+c = 4.5
+print(a * b * c)
+
+output= 160
+````
+##ejercicio 3
+````python
+a= 4
+b= 3
+x= 4*3
+print(x)
+
+output= 12
+````
