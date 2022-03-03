cd579a0632c51941c26c1e8ee234de267cfc25c0
Say133 <99740761+Say133@users.noreply.github.com>
2022-03-03 17:19:02 -0500

    resta

---
+++ operadores básicos/resta/resta.py
@@ -0,0 +1,26 @@
+##ejercicio 1
+````python
+r1= 4
+r2= 2
+print(r1-r2)
+
+output= 2 
+````
+##Ejercicio 2
+````python
+a = 10
+b = 4
+c = 4.5
+print(a - b - c)
+
+output= 1.5
+
+##ejercicio 3
+````python
+x1=6
+x2=3
+resta=6-3
+print(resta)
+
+output= 3
+````
