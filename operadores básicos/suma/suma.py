75b13f513e36cdf0ac028037219b0e664a9b10ab
Say133 <99740761+Say133@users.noreply.github.com>
2022-03-03 17:15:33 -0500

    suma

--- operadores básicos/suma/suma.py
+++ operadores básicos/suma/suma.py
@@ -1,18 +1,18 @@
-Ejercicio 1
+##Ejercicio 1
 ````python 
 sum1=3
 sum2=4
 print(sum1+sum2) 
 output=7
 ````
-ejercicio 2
+##ejercicio 2
 ````python
 edades=12,13,14,22,12,10.11
 suma_edades=sum(edades)
 print(suma_edades)
 outout=94
 ````
-Ejercicio 3
+##Ejercicio 3
 ````python
 a = 10
 b = 4
