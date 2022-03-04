d9472bd1d38edc2638fc97ff7c58e7ad55be7d96
Say133 <99740761+Say133@users.noreply.github.com>
2022-03-03 20:04:28 -0500

    potencia

--- operadores básicos/Potencia/potencia.py
+++ operadores básicos/Potencia/potencia.py
@@ -1,8 +1,22 @@
 #Ejercicio 1
-````python
 p1=4
 p2=2
 x= 4**2
 print(x)
-output= 16 
-````
+
+output= 16
+
+#ejercicio 2
+num1=8+2
+num2= 3
+print(num1**num2)
+
+output=1000
+
+#ejercicio 3
+a=20
+b=5 
+c= 20-15
+print(c**b)
+output= 3125
+
