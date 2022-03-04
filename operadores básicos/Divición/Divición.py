a954a934d08162487ecde54a18af6cdfbc4ef27d
Say133 <99740761+Say133@users.noreply.github.com>
2022-03-03 19:52:21 -0500

    Divición

--- operadores básicos/Divición/Divición.py
+++ operadores básicos/Divición/Divición.py
@@ -1,17 +1,20 @@
 #ejercicio 1
-````python
-"/"Division entre los operadores
+
 d1=8
 d2=2
 print(d1/d2)
-output= 4
-````
+output=  4
+
 #ejercicio 2
-````python
-````
+a=(10/2)
+b=20
+print(b/a)
+output= 4
+
 #ejercicio 3
-````python
 a= 5
 b= 3
+c= 5*3
+print(c/a)
+output= 3
 
-````
