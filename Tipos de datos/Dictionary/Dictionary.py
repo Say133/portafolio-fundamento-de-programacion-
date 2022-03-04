df3f152dc8355fa953dd488d3a552221ea4d389f
Say133 <99740761+Say133@users.noreply.github.com>
2022-03-04 18:51:19 -0500

    dictionary

--- Tipos de datos/Dictionary/Dictionary.py
+++ Tipos de datos/Dictionary/Dictionary.py
@@ -1,1 +1,20 @@
+#ejercicio 1
+diccionario = {
+        'int': 10,
+        'str': 'Hola3
+        'float':8.0
+        }
+print(diccionario)
 
+# ejercicio 2
+#Escribir un programa que guarde en una variable el diccionario
+#{'Euro':'€', 'Yen':'¥'}, pregunte al usuario por una 
+#divisa" moneda" y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.
+monedas = {'Euro':'€', 'Yen':'¥'}
+moneda = input("Introduce una divisa: ")
+if moneda.title() in monedas:
+    print(monedas[moneda.title()])
+else:
+    print("La divisa no está.")
+    
+#Ejercicio 3
