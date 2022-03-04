05c104508a6d19f2e49bc4903737f2b9784180bd
Say133 <99740761+Say133@users.noreply.github.com>
2022-03-03 20:17:19 -0500

    for

--- Tomando deciciones/ciclo for/for.py
+++ Tomando deciciones/ciclo for/for.py
@@ -1,1 +1,8 @@
-
+# ejercicio 1
+calcular e imprimir la tabla de multiplicar de un numero cualquiera.
+imprimir el multiplicando, el multiplicador y el producto 
+n=12
+for r in range (1,12):
+    print(f"{r} x  {n} =  {r * n}")
+   
+#ejercicio 2 
