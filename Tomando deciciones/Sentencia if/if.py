673d118a8ac2db34620ca72cbfe509742eb4dcb0
Say133 <99740761+Say133@users.noreply.github.com>
2022-03-04 19:52:47 -0500

    if

--- Tomando deciciones/Sentencia if/if.py
+++ Tomando deciciones/Sentencia if/if.py
@@ -1,3 +1,26 @@
 # ejercicio 1
-# verificar si en numero es positivo o negativo
+# Realizar un programa que pregunte la edad y muestre por pantalla si es mayor de edad o no.
+edad = int(input("ingrese edad:"))
+if edad < 18: 
+    print ("Eres menor de edad.")
+else:
+    print("Eres mayor de edad.")
+    
+#ejercicio 2
+#realize una programa en donde se ingrese un número y verificamos si es par o impar
+número= int(input("Introduce un número entero: "))
+if n % 2 == 0:
+    print("El número " + str(n) + " es par")
+else:
+    print("El número " + str(n) + " es impar")
+    
+#ejercicio 3 
+#solicitar y ver si el numero entero es positivo o negativo
+num=-20
 
+if num==0:
+    print("cero")
+if num>0:
+    print("es positivo")
+if num<0:
+    print("es negativo")
