d4b51971074927c07f74d7e5a5a9773d0f8d68dc
Say133 <99740761+Say133@users.noreply.github.com>
2022-03-04 21:06:59 -0500

    string

--- Tipos de datos/String/String.py
+++ Tipos de datos/String/String.py
@@ -5,6 +5,11 @@ y = "gato"
 print("mis mascotas:", x,"y", y)
 
 #ejercicio 2
-
+#contar las palabras en un ensayo y ver si el ensayo es largo o corto
+s = input("Ingresa tu ensayo:")
+if len(s)>140:
+     print("Este ensayo es demasiado largo!")
+elif len(s)<100:
+    print("este ensayo es corto!")
 
 #ejercicio 3
