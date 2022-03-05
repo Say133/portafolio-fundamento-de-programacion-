88108a805daeb0279788c3c275961b0a2ec35f28
Say133 <99740761+Say133@users.noreply.github.com>
2022-03-04 21:13:55 -0500

    Tuple

--- Tipos de datos/Tuple/Tuple.py
+++ Tipos de datos/Tuple/Tuple.py
@@ -1,7 +1,18 @@
-#
+#ejercicio 1
+tuple = (1, 2, 3)
+print(tuple) 
 
 #ejercicio 2
+lst1 = [10,20,30]
+lst2 = [50,"Python","jami"]
+lst_tuple = list(zip(lst1,lst2))
+print(lst_tuple)
+ 
+  
+#ejercicio 3
 lst = [13,"Python","sayla",19]
 lst_tuple = [x for x in zip(*[iter(lst)])]
-
 print(lst_tuple
+      
+
+      
